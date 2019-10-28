b01275ab606a4fd54dc79c01beb701650821d0c3
Joel Grissom <joelgrissom5@gmail.com>
2019-10-28 00:32:06 -0700

    feat: added mvc structure

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 const path = require('path')
+
 const express = require('express')
-const adminData = require('./routes/admin') 
-const shopRoutes = require('./routes/shop')
 const bodyParser = require('body-parser')
+
+const errorController = require('./controllers/error')
 
 const app = express()
 
-
-
 app.set('view engine', 'ejs')
-// app.set('view engine', 'pug')
 app.set('views', 'views')
+
+const adminRoutes = require('./routes/admin') 
+const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: '404 Page', pageTitle: 'Page Not Found' })
-})
+app.use(errorController.get404)
 
 const port = 3000
 console.log(`listening on port ${port}`)
 
-app.listen(3000)
+app.listen(port)
