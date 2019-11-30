e02940ed1b2925dffbbd8dd445842ca73b263153
Joel Grissom <joelgrissom5@gmail.com>
2019-11-30 00:39:38 -0700

    feat: created dynamic routes and advanced models

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-
+const Cart = require('./cart')
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -18,7 +18,8 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id
     this.title = title
     this.imageUrl = imageUrl
     this.description = description
@@ -27,14 +28,45 @@ module.exports = class Product {
 
   save() {
     getProductsFromFile(products => {
-      products.push(this)
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err)
+      if (this.id) {
+        const exstingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+        )
+        const updatedProducts = [...products]
+        updatedProducts[exstingProductIndex] = this
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err)
+        })
+      } else {
+        this.id = Math.random().toString()
+        products.push(this)
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err)
+        })
+      }
+    })
+  }
+
+  static deleteById(id) {
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id)
+      const updatedProducts = products.filter(prod => prod.id !== id)
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if (!err) {
+          Cart.deleteProduct(id, product.price)
+        }
       })
     })
   }
 
   static fetchAll(cb) {
     getProductsFromFile(cb)
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id)
+      cb(product)
+    })
   }
 }
